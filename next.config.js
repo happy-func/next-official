820a977962ae4d512088fa3754ef755b910bf369
zzx <zhaoxiang.zhang@surpath.net>
2021-08-06 17:43:18 +0800

    locale && prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   i18n: {
-    locales: ['en-US', 'zh-CN'],
-    defaultLocale: 'zh-CN',
+    locales: ["en-US", "zh-CN"],
+    defaultLocale: "zh-CN",
   },
-}
+};
