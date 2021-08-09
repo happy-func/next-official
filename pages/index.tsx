f499818ee856195cd4e38756ccebeeeb164f467f
zzx <zhaoxiang.zhang@surpath.net>
2021-08-09 18:11:43 +0800

    use sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import { useEffect, useState } from "react";
 
 export default function Home() {
