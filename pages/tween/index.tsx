346f463e9d65355ef993f0479b62727e27326a8a
zzx <zhaoxiang.zhang@surpath.net>
2021-08-17 16:20:01 +0800

    chain tween

--- pages/tween/index.tsx
+++ pages/tween/index.tsx
@@ -1,43 +1,64 @@
 import styles from "./index.module.scss";
 import PageTitle from "../../components/title";
 import TWEEN from "@tweenjs/tween.js";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+
+interface DEST {
+  x: number;
+  y: number;
+}
 
 export default function TweenPage() {
-  const coords = {
+  const coords: DEST = {
     x: 0,
     y: 0,
   };
+  const box = useRef(null);
   // Setup the animation loop.
   function animate(time: number | undefined) {
     requestAnimationFrame(animate);
     TWEEN.update(time);
   }
-  useEffect(() => {
-    const box = document.querySelector("#animateBox");
-    new TWEEN.Tween(coords)
+  function updateProperty() {
+    // @ts-ignore
+    box.current.style.setProperty(
+      "transform",
+      `translate(${coords.x}px, ${coords.y}px)`
+    );
+  }
+  function initTween() {
+    const tweenA = new TWEEN.Tween(coords)
       .to({ x: 300, y: 0 }, 1000)
       .easing(TWEEN.Easing.Cubic.Out) // Use an easing function to make the animation smooth.
-      .onUpdate(() => {
-        // @ts-ignore
-        box.style.setProperty(
-          "transform",
-          `translate(${coords.x}px, ${coords.y}px)`
-        );
-      })
-      .onComplete(({x, y}) => {
-        if (x === 300 && y === 0) {
-          coords.x = 300;
-          coords.y = 300;
-        }
-      })
-      .start(); // Start the tween immediately.
+      .onUpdate(updateProperty);
+    const tweenB = new TWEEN.Tween(coords)
+      .to({ x: 300, y: 300 }, 1000)
+      .easing(TWEEN.Easing.Cubic.Out) // Use an easing function to make the animation smooth.
+      .onUpdate(updateProperty);
+    const tweenC = new TWEEN.Tween(coords)
+      .to({ x: 0, y: 300 }, 1000)
+      .easing(TWEEN.Easing.Cubic.Out) // Use an easing function to make the animation smooth.
+      .onUpdate(updateProperty);
+    const tweenD = new TWEEN.Tween(coords)
+      .to({ x: 0, y: 0 }, 1000)
+      .easing(TWEEN.Easing.Cubic.Out) // Use an easing function to make the animation smooth.
+      .onUpdate(updateProperty)
+      .onComplete(() => {
+        tweenA.start();
+      });
+    tweenA.chain(tweenB);
+    tweenB.chain(tweenC);
+    tweenC.chain(tweenD);
+    tweenA.start();
+  }
+  useEffect(() => {
+    initTween();
     requestAnimationFrame(animate);
   }, []);
   return (
     <div className={styles.animateWrap}>
       <PageTitle title="tweenjs 实践" />
-      <div className={styles.animateBox} id="animateBox" />
+      <div className={styles.animateBox} id="animateBox" ref={box} />
     </div>
   );
 }
